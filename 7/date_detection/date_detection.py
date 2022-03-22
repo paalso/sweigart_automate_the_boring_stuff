a23305da8f0451e87437d0260c35226a106368b9
paalso <40a.pvl@gmail.com>
2022-03-22 14:28:34 +0200

    date_detection in progress

--- 7/date_detection/date_detection.py
+++ 7/date_detection/date_detection.py
@@ -10,32 +10,47 @@
 import re
 import sys
 
+POSSIBLE_DATE_REGEX = re.compile(r'''
+    (0?[1-9]|[12][0-9]|3[01])
+    (\-|/)
+    (0?[1-9]|1[012])
+    \2
+    (\d{4})
+''', re.VERBOSE)
+
 
 def normalize_date(day, month, year):
-    return f'{day:02d}/{month:02d}{year}'
+    return f'{day:02d}/{month:02d}/{year}'
+
+
+def check_date(day, month, year):
+    day, month, year = int(day), int(month), int(year)
+    if year < 1000 or 2999 < year:
+        return False
+    if month in (4, 6, 9, 11):
+        return 1 <= day <= 30
+    if month == 2:
+        return 1 <= day <= (29 if is_leap_year(year) else 28)
+    return 1 <= day <= 31
 
 
 def is_leap_year(year):
-    return year % 400 == 0 or (year % 00 != 0 and year % 4 == 0)
-
-
-def refine_check_date(day, month, year):
-    day, month, year = int(day), int(month), int(year)
-    if month in (4, 6, 9, 11):
-        return 1 <= day <= 30
-    return 1 <= day <= (29 if is_leap_year(year) else 28)
-
+    return year % 400 == 0 or (year % 100 != 0 and year % 4 == 0)
 
 
 def parse_args():
     if len(sys.argv) != 2:
         print('Usage: {sys.argv[0]} filename')
         sys.exit(1)
-    return sys.argv
+    return sys.argv[1]
 
 
 def main():
-    filename = parse_args[1]
+    filename = parse_args()
+    all_dates = []
+    with open(filename, 'r') as f:
+        for line in f:
+            pass
 
 
 if __name__ == '__main__':
