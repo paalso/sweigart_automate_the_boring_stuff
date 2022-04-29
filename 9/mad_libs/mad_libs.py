ffd37c74050c3a1a17b341abd1977c4b6aede4c8
paalso <40a.pvl@gmail.com>
2022-04-29 12:58:10 +0300

    Chapter 9. 'Reading and wrining files'. Complete project Mad Libs

--- 9/mad_libs/mad_libs.py
+++ 9/mad_libs/mad_libs.py
@@ -3,30 +3,26 @@
 import sys
 import re
 
-KEYWORDS = ['adjective', 'noun', 'verb']
+KEYWORDS = ["adjective", "noun", "verb"]
 
 
-def read_keywords(keywords):
-    print('Input replacements for given keywods')
-    return {keyword: input(f'Enter an {keyword}: ') for keyword in keywords}
-
-
-def replace_keywords(filename, replacements, case_insensitive=True):
-        def handle_line(line):
-            for word in replacements:
-                replacement = replacements[word]
-                regex = re.compile(word, re.I) if case_insensitive \
-                        else re.compile(word)
-                return regex.sub(replacement, line)
-
-        lines = open(filename,'r').readlines()
-        open(filename,'w').writelines((map(handle_line, lines)))
+def sub_keywords_in_line(line, keywords):
+    regex = re.compile(
+        r"{}".format("|".join(map(lambda e: f"\\b{e}\\b", keywords))), re.I
+    )
+    for e in regex.finditer(line):
+        replacement = input(f"Enter an {e.group()}: ")
+        line = regex.sub(replacement, line, count=1)
+    return line
 
 
 def main():
-    replaсements = read_keywords(KEYWORDS)
-    replace_keywords(sys.argv[1], replaсements)
+    filename = sys.argv[1]
+    lines = open(filename, "r").readlines()
+    open(filename, "w").writelines(
+        map(lambda line: sub_keywords_in_line(line, KEYWORDS), lines)
+    )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
