a3474a0a71a2f1c32d7db1ac1accf443c0f84e1e
Pavel Soroka <40a.pvl@gmail.com>
2023-07-23 15:45:28 +0300

    Change filename output

--- 9/regex_search/regfind.py
+++ 9/regex_search/regfind.py
@@ -9,7 +9,7 @@ import colorama
 import os
 import re
 
-DESCRIPTION = \
+PARSER_DESCRIPTION = \
 '''Searches for any line that matches a user-supplied regular expression in
 all files in a folder that matches a given file mask'''
 
@@ -20,7 +20,7 @@ FILENAME_LINE_SEPARATOR_COLOR = colorama.Fore.MAGENTA
 
 
 def get_args():
-    parser = argparse.ArgumentParser(DESCRIPTION)
+    parser = argparse.ArgumentParser(PARSER_DESCRIPTION)
 
     parser.add_argument(
         'mask',
@@ -35,38 +35,46 @@ def get_args():
     return args.mask, args.path
 
 
-def print_line(file, line, regex):
+def print_line(line, regex, filename, first_find_flag):
     mask_chunks = regex.findall(line)
     split_text_chunks = regex.split(line)
-    if mask_chunks:
-        print(FILENAME_COLOR +
-              f'{file}' +
-              colorama.Style.RESET_ALL, end='')
 
-        print(FILENAME_LINE_SEPARATOR_COLOR +
-              FILENAME_LINE_SEPARATOR +
-              colorama.Style.RESET_ALL, end='')
+    if first_find_flag:
+        print_filename(filename)
 
-        print(split_text_chunks[0] +
-              colorama.Style.RESET_ALL, end='')
+    print(split_text_chunks[0] +
+          colorama.Style.RESET_ALL, end='')
 
-        for text_chunk, matched_chunk in zip(split_text_chunks[1:],
-                                             mask_chunks):
-            print(MASK_CHUNK_COLOR +
-                  matched_chunk +
-                  colorama.Style.RESET_ALL, end='')
-            print(text_chunk, end='')
-        print(colorama.Style.RESET_ALL)
+    for text_chunk, matched_chunk in zip(split_text_chunks[1:],
+                                         mask_chunks):
+        print(MASK_CHUNK_COLOR +
+              matched_chunk +
+              colorama.Style.RESET_ALL, end='')
+        print(text_chunk, end='')
+    print(colorama.Style.RESET_ALL)
 
 
 def print_file_matched_lines(file, regex):
+    first_find_flag = True
     with open(file, 'r') as f:
         for line in f:
-            print_line(file, line.rstrip(), regex)
+            if is_mask_in_line(line, regex):
+                print_line(line.rstrip(), regex, file, first_find_flag)
+                first_find_flag = False
+
+
+def is_mask_in_line(line, regex):
+    return regex.search(line)
 
 
 def print_filename(filename):
-    print(filename)
+    print(FILENAME_COLOR +
+          f'{filename}' +
+          colorama.Style.RESET_ALL, end='')
+
+    print(FILENAME_LINE_SEPARATOR_COLOR +
+          FILENAME_LINE_SEPARATOR +
+          colorama.Style.RESET_ALL)
 
 
 def main():
