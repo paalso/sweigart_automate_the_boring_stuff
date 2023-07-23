9ccac37a004f73e6c695290038dfff7c3e7ce5bd
Pavel Soroka <40a.pvl@gmail.com>
2023-07-23 15:50:29 +0300

    Change the file selection logic to use glob functionality

--- 9/regex_search/regfind.py
+++ 9/regex_search/regfind.py
@@ -6,6 +6,7 @@
 
 import argparse
 import colorama
+import glob
 import os
 import re
 
@@ -13,7 +14,7 @@ PARSER_DESCRIPTION = \
 '''Searches for any line that matches a user-supplied regular expression in
 all files in a folder that matches a given file mask'''
 
-MASK_CHUNK_COLOR = colorama.Fore.RED
+REGEX_CHUNK_COLOR = colorama.Fore.RED
 FILENAME_COLOR = colorama.Fore.GREEN
 FILENAME_LINE_SEPARATOR = ':'
 FILENAME_LINE_SEPARATOR_COLOR = colorama.Fore.MAGENTA
@@ -23,20 +24,20 @@ def get_args():
     parser = argparse.ArgumentParser(PARSER_DESCRIPTION)
 
     parser.add_argument(
-        'mask',
-        help='regular expression mask to search in the specified files')
+        'regex',
+        help='regular expression to search in the specified files')
 
     parser.add_argument(
         '-p', '--path',
-        default='.',
+        default='*',
         help='path to the files to search, default is current directory')
 
     args = parser.parse_args()
-    return args.mask, args.path
+    return args.regex, args.path
 
 
 def print_line(line, regex, filename, first_find_flag):
-    mask_chunks = regex.findall(line)
+    regex_chunks = regex.findall(line)
     split_text_chunks = regex.split(line)
 
     if first_find_flag:
@@ -46,8 +47,8 @@ def print_line(line, regex, filename, first_find_flag):
           colorama.Style.RESET_ALL, end='')
 
     for text_chunk, matched_chunk in zip(split_text_chunks[1:],
-                                         mask_chunks):
-        print(MASK_CHUNK_COLOR +
+                                         regex_chunks):
+        print(REGEX_CHUNK_COLOR +
               matched_chunk +
               colorama.Style.RESET_ALL, end='')
         print(text_chunk, end='')
@@ -58,12 +59,12 @@ def print_file_matched_lines(file, regex):
     first_find_flag = True
     with open(file, 'r') as f:
         for line in f:
-            if is_mask_in_line(line, regex):
+            if is_regex_in_line(line, regex):
                 print_line(line.rstrip(), regex, file, first_find_flag)
                 first_find_flag = False
 
 
-def is_mask_in_line(line, regex):
+def is_regex_in_line(line, regex):
     return regex.search(line)
 
 
@@ -78,14 +79,14 @@ def print_filename(filename):
 
 
 def main():
-    mask, path = get_args()
-    regex = re.compile(mask)
+    regex, template_path = get_args()
+    regex = re.compile(regex)
 
     colorama.init()
 
-    for file in os.listdir(path):
-        if os.path.isfile(file):
-            print_file_matched_lines(file, regex)
+    for path in sorted(glob.glob(template_path)):
+        if os.path.isfile(path):
+            print_file_matched_lines(path, regex)
 
 
 if __name__ == '__main__':
