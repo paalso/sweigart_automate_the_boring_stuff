cf38f5e41c24f24421100ab1aadc69dec1fac431
Pavel Soroka <40a.pvl@gmail.com>
2023-08-12 17:35:01 +0300

    Complete Mini-Project: Renaming Files between American-Style and European-Style Dates

--- 10-Organizing_Files/renaming-files-with-american-style-dates-to-european-style-dates/rename.py
+++ 10-Organizing_Files/renaming-files-with-american-style-dates-to-european-style-dates/rename.py
@@ -2,12 +2,13 @@
 
 '''
 rename_dates.py
-Renames filenames with American MM-DD-YYYY date format # to European DD-MM-YYYY
+Renames filenames between American MM-DD-YYYY date and European DD-MM-YYYY one
 '''
 
+import argparse
 import re
-import os
 import shutil
+from pathlib import Path
 
 
 DATE_PATTERN = re.compile(
@@ -17,6 +18,20 @@ DATE_PATTERN = re.compile(
     ((19|20)\d\d)                     # four digits for the year
     (.*?)$                            # all text after the date
     """, re.VERBOSE)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='Renames filenames between American MM-DD-YYYY date' +
+                    ' and European DD-MM-YYYY one')
+
+    parser.add_argument('path', nargs='?', default='.')
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-eu', '--european', action='store_true')
+    group.add_argument('-us', '--united_states', action='store_true')
+
+    return parser
 
 
 def check_filename_for_needed_date_style(filename, pattern):
@@ -39,20 +54,40 @@ def rename_recomposing_date_format(matching, format='eu'):
     return f"{prefix}{'-'.join(new_date_tokens)}{suffix}"
 
 
-def process_dir(pattern, dirname='.'):
-    for filename in os.listdir('.'):
-        full_filename = os.path.abspath(filename)
+def get_full_dir_path(filename):
+    file_path = Path(filename)
+    if Path.is_absolute(file_path):
+        return file_path
+    return Path.cwd() / filename
+
+
+def process_dir(pattern, dirpath, format='eu'):
+    cwd = Path.cwd()
+    for path in Path.iterdir(dirpath):
+        filename = path.name
         matching = check_filename_for_needed_date_style(
-            filename, pattern)
+                    filename, pattern)
         if matching:
-            new_filename = rename_recomposing_date_format(matching)
-            new_full_filename = os.path.abspath(new_filename)
-            print(f'renaming {filename} -> {new_filename}')
-            shutil.move(full_filename, new_full_filename)
+            new_filename = rename_recomposing_date_format(matching, format)
+            new_path = dirpath / new_filename
+            print(f'renaming {path.relative_to(cwd)} -> ' +
+                  f'{new_path.relative_to(cwd)}')
+            shutil.move(path, new_path)
 
 
 def main():
-    process_dir(DATE_PATTERN)
+    parser = get_parser()
+    args = parser.parse_args()
+    dirname = args.path
+    date_format = 'eu' if args.european else 'us'
+
+    dirpath = get_full_dir_path(dirname)
+
+    if not Path.exists(dirpath):
+        print(f'Error: {dirname} does not exist')
+        exit(2)
+
+    process_dir(DATE_PATTERN, dirpath, date_format)
 
 
 if __name__ == '__main__':
